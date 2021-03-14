513adc515f4d15693e2b2bd800b96ebb12d3acc6
Rached <archer193@hotmail.com>
2021-03-14 23:53:22 +0100

    Añadir configuraciones de enviroment

---
+++ ormconfig.ts
@@ -0,0 +1,16 @@
+import PgConf  from './hidden.json';
+export = {
+    type: "postgres",
+    username: process.env.DATABASE_USERNAME || PgConf.username,
+    host: process.env.DATABASE_HOST || PgConf.host,
+    database: process.env.DATABASE || PgConf.database,
+    password: process.env.PASSWORD_PASSWORD || PgConf.password,
+    port: 5432,
+    entities: [
+        "enties/**/*.ts"
+    ],
+    ssl: {
+        rejectUnauthorized: false
+    },
+    synchronize: true
+}
